195c6845f84e37f42aafb4c10ce0102868abe5a4
CVVlizok <lizaagafonova8@gmail.com>
2024-02-19 09:56:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a1p/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a1p;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity{
+    private Button button;
+    private TextView textView;
+    public final static String EXTRA_MESSAGE = "com.example.a1p.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View v){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
